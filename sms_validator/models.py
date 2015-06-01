8397b697b4f89e7176cc14509626c84d32e5a7f2
ondrejSkowronek <ondrej.skowronek@gmail.com>
2015-06-01 15:58:40 +0200

    Deactivate token change
    SMSTokenManager do not deactivate token when return if token is valid

--- sms_validator/models.py
+++ sms_validator/models.py
@@ -36,11 +36,15 @@ class SMSTokenManager(models.Manager):
         token = get_object_or_none(self.model, validating_type=ContentType.objects.get_for_model(obj),
                                    validating_id=obj.pk, is_active=True, key=key, slug=slug)
         if token is not None and not token.is_expired:
-            token.is_active = False
-            token.save()
             return True
         else:
             return False
+
+    def deactivate_tokens(self, obj, slug=None):
+        for token in self.filter(validating_type=ContentType.objects.get_for_model(obj), is_active=True,
+                                 validating_id=obj.pk, slug=slug):
+            token.is_active = False
+            token.save()
 
     def count_tokens(self, obj, slug=None):
         """
